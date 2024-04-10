f38e7c909c6c70f98d14ed86ba6a386291b37590
John Afanasieff <118971918+Wafflelover404@users.noreply.github.com>
2024-04-10 11:14:22 +0300

    Add files via upload

---
+++ merge.py
@@ -0,0 +1,20 @@
+import random
+
+def merge_html_files(header_file_path, body_file_path, output_file_path):
+    with open(header_file_path, 'r') as header_file, open(body_file_path, 'r') as body_file:
+        header = header_file.read()
+        body = body_file.read()
+
+    merged_html = header + body
+
+    with open(output_file_path, 'w') as output_file:
+        output_file.write(merged_html)
+
+header_path_number = random.randint(1, 5)
+body_path_number = random.randint(1, 1)
+print(header_path_number , body_path_number)
+
+header_path = f'./Header/ex{header_path_number}/index.html'
+body_path = f'./Body/ex{body_path_number}/index.html'
+
+merge_html_files('./Header/ex1/index.html', './Body/ex1/index.html', 'output.html')
