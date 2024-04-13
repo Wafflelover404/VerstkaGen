a94ddeb29fbe09a8ea22ab766e7482444f921431
John Afanasieff <118971918+Wafflelover404@users.noreply.github.com>
2024-04-13 11:27:31 +0300

    Update search_image.py

--- search_image.py
+++ search_image.py
@@ -1,12 +1,14 @@
 import requests
+import random
 
-def get_image_urls(query, orientation="horizontal"):
-    API_KEY = ""  # replace with your Pixabay API key
+def get_image_urls(query, orientation):
+    API_KEY = "your key"  # replace with your Pixabay API key
     url = f"https://pixabay.com/api/?key={API_KEY}&q={query}&image_type=photo&orientation={orientation}"
     response = requests.get(url)
     data = response.json()
     urls = [img["webformatURL"] for img in data["hits"]]
     return urls
 
-
-print(get_image_urls("Coffee", "vertical"))
+topic = input("Topic to search for ~> ")
+print("vertical ~> ", random.choice(get_image_urls(topic, "vertical")))
+print("horizontal ~> ", random.choice(get_image_urls(topic, "horizontal")))
