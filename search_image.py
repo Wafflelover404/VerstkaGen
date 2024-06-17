b3d09ff7b135b33927b019b04116024dccc1c537
John Afanasieff <118971918+Wafflelover404@users.noreply.github.com>
2024-06-17 21:17:24 +0300

    Update search_image.py

--- search_image.py
+++ search_image.py
@@ -2,7 +2,7 @@ import requests
 import random
 
 def get_image_urls(query, orientation):
-    API_KEY = "43328343-b9278c71ab9b02120824b9bdc"  # replace with your Pixabay API key
+    API_KEY = ""  # replace with your Pixabay API key
     url = f"https://pixabay.com/api/?key={API_KEY}&q={query}&image_type=photo&orientation={orientation}"
     response = requests.get(url)
     data = response.json()
